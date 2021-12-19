570645b90c20e8b59184e7c7638237e3be68fa14
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-12-19 11:27:56 +0100

    Projektdateien hinzufügen.

---
+++ sfmlPhysicstest/sfmlPhysicstest.cpp
@@ -0,0 +1,12 @@
+﻿// sfmlPhysicstest.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "sfmlPhysicstest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
